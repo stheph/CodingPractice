6bccec23cd5be470bd322bf09ef76ac3da720e43
Steven Thephsourinthone <zx.steven.t@gmail.com>
2022-07-18 20:49:46 -0400

    Added test_extract_min_structure(), fixed previous tests

--- MinHeap_python/MinHeap_test.py
+++ MinHeap_python/MinHeap_test.py
@@ -4,7 +4,7 @@ import numpy as np
 def test_create_new_heap():
     test = MH.MinHeap()
     # Backing array should be equivalent to an empty array of size HEAP_SIZE
-    assert all(map(lambda x: x[0] == x[1], (zip(test.as_array, np.zeros((MH.HEAP_SIZE))))))
+    assert all(map(lambda x: x[0] == x[1], (zip(test.as_array(), np.zeros((MH.HEAP_SIZE))))))
 
 def test_insert_one_element_into_empty_heap():
     test = MH.MinHeap()
@@ -13,7 +13,7 @@ def test_insert_one_element_into_empty_heap():
     test_against_array = np.zeros((MH.HEAP_SIZE))
     test_against_array[0] = 1
 
-    assert all(map(lambda x: x[0] == x[1], (zip(test.as_array, test_against_array))))
+    assert all(map(lambda x: x[0] == x[1], (zip(test.as_array(), test_against_array))))
 
 def test_insert_multiple_elements_into_empty_heap():
     test = MH.MinHeap()
@@ -40,7 +40,37 @@ def test_insert_multiple_elements_into_empty_heap():
     test_against_array[2] = 7
     test_against_array[3] = 10
 
-    assert all(map(lambda x: x[0] == x[1], (zip(test.as_array, test_against_array))))
+    assert all(map(lambda x: x[0] == x[1], (zip(test.as_array(), test_against_array))))
+
+def test_extract_min_structure():
+    # Same as test_insert_multiple_elements_into_empty_heap() to set up the tree for now/until we implement heap building function
+    test = MH.MinHeap()
+    test.insert(10)
+    # 10 is root
+    test.insert(5)
+    # Add 5 to left child, 5 and 10 swap
+    test.insert(7)
+    # Add 7 as right child
+    test.insert(1)
+    # Add 1 as left-left child, swaps with 10, then 5.
+
+    # Final Tree:
+    #       1
+    #      / \
+    #     5   7
+    #    /
+    #  10
+    # [1,5,7,10]
+
+    # TODO Check that temp is correct later
+    temp = test.extract_min()
+
+    test_against_array = np.zeros((MH.HEAP_SIZE))
+    test_against_array[0] = 5
+    test_against_array[1] = 10
+    test_against_array[2] = 7
+
+    assert all(map(lambda x: x[0] == x[1], (zip(test.as_array(), test_against_array))))
 
 if __name__ == "__main__":
     test_create_new_heap()
@@ -51,3 +81,6 @@ if __name__ == "__main__":
 
     test_insert_multiple_elements_into_empty_heap()
     print ("test_insert_multiple_elements_into_empty_heap passed!")
+
+    test_extract_min_structure()
+    print ("test_extract_min_structure passed!")
