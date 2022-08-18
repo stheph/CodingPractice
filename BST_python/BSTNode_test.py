b8dff20cd0d8ee97737498e241a8b01e1729d9a7
Steven Thephsourinthone <zx.steven.t@gmail.com>
2022-08-18 18:10:42 -0400

    Added create/insert tests

--- BST_python/BSTNode_test.py
+++ BST_python/BSTNode_test.py
@@ -6,28 +6,63 @@ def build_tree(l):
         BST.insert(tree, BST.BSTNode(e, ""))
     return tree
 
+def test_create_bst():
+    tree = BST.BSTNode(1, "")
 
-tree = BST.BSTNode(12, "")
+    inorder = BST.inorder(tree) # [1]
+    key = tree.get_key() # 1
+    data = tree.get_data() # ""
+    left_child = tree.get_left_child() # None
+    right_child = tree.get_right_child() # None
+    parent = tree.get_parent() # None
 
-node5 = BST.BSTNode(5, "")
-node18 = BST.BSTNode(18, "")
-node2 = BST.BSTNode(2, "")
-node9 = BST.BSTNode(9, "")
-node15 = BST.BSTNode(15, "")
-node19 = BST.BSTNode(19, "")
-node13 = BST.BSTNode(13, "")
-node17 = BST.BSTNode(17, "")
+    assert ((inorder, key, data, left_child, right_child, parent) == ([1], 1, "", None, None, None))
 
-BST.insert(tree, node5)
-BST.insert(tree, node18)
-BST.insert(tree, node2)
-BST.insert(tree, node9)
-BST.insert(tree, node15)
-BST.insert(tree, node19)
-BST.insert(tree, node13)
-BST.insert(tree, node17)
+def test_insert_left_node():
+    tree = BST.BSTNode(2, "test2")
+    node = BST.BSTNode(1, "test1")
+    BST.insert(tree, node)
 
-print(BST.inorder(tree))
+    inorder = BST.inorder(tree) # [1,2]
 
-tree2 = build_tree([12,5,18,2,9,15,19,13,17])
-print(BST.inorder(tree2))
+    key = tree.get_key() # 2
+    data = tree.get_data() # "test2"
+    left_child = tree.get_left_child() # node
+    right_child = tree.get_right_child() # None
+    parent = tree.get_parent() # None
+
+    left_child_key = left_child.get_key() # 2
+    left_child_data = left_child.get_data() # "test2"
+
+    assert ((inorder, key, data, left_child, right_child, parent, left_child_key, left_child_data) \
+        == ([1,2], 2, "test2", node, None, None, 1, "test1"))
+
+
+def test_insert_right_node():
+    tree = BST.BSTNode(1, "test1")
+    node = BST.BSTNode(2, "test2")
+    BST.insert(tree, node)
+
+    inorder = BST.inorder(tree) # [1,2]
+
+    key = tree.get_key() # 1
+    data = tree.get_data() # "test1"
+    left_child = tree.get_left_child() # None
+    right_child = tree.get_right_child() # node
+    parent = tree.get_parent() # None
+
+    right_child_key = right_child.get_key() # 2
+    right_child_data = right_child.get_data() # "test2"
+
+    assert ((inorder, key, data, left_child, right_child, parent, right_child_key, right_child_data) \
+        == ([1,2], 1, "test1", None, node, None, 2, "test2"))
+
+if __name__ == "__main__":
+    test_create_bst()
+    print ("test_create_bst passed!")
+
+    test_insert_left_node()
+    print ("test_insert_left_node passed!")
+
+    test_insert_right_node()
+    print ("test_insert_right_node passed!")
