d7aaebaeb0d5ac1b6c3f7fb036a3bed75fe360e7
Steven Thephsourinthone <zx.steven.t@gmail.com>
2022-08-30 11:31:37 -0400

    Working on split function

--- MergeSort_ocaml/MergeSort.ml
+++ MergeSort_ocaml/MergeSort.ml
@@ -9,3 +9,16 @@ let rec merge l1 l2 =
       then x::(merge xs (y::ys))
       else y::(merge (x::xs) ys)
   end
+
+let rec split l =
+  begin
+    match l with
+    | [] -> ([], [])
+    | [x] -> ([x], [])
+    | hd::tl ->
+      (* split [1;2] -> split [2] -> [2], [] -> [2], 1::[] *)
+        let (l1, l2) = split tl in
+        (l1, hd::l2)
+  end
+
+let (l1, l2) = split [1;2;3;4;5;6;7;8;9]
