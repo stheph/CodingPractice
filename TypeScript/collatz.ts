7cd39b630d73a4385cd86001ab81abd5fe10786b
Steven Thephsourinthone <zx.steven.t@gmail.com>
2022-09-04 00:09:57 -0400

    Updated collatz to trace instead of just returning the number of steps

--- TypeScript/collatz.ts
+++ TypeScript/collatz.ts
@@ -1,7 +1,7 @@
-let memo = new Map<number, number>
-memo.set(1, 0)
+let memo = new Map<number, number[]>
+memo.set(1, [1])
 
-function collatz (n : number) : number 
+function collatz (n : number) : number[]
 {
     if (memo.has(n))
     {
@@ -11,15 +11,15 @@ function collatz (n : number) : number
     {
         if (n % 2 == 0)
         {
-            let previousSteps : number = collatz(n / 2);
-            memo.set(n, 1 + previousSteps);
-            return 1 + previousSteps;
+            let previousSteps : number[] = collatz(n / 2);
+            memo.set(n, [n, ...previousSteps]);
+            return [n, ...previousSteps];
         }
         else
         {
-            let previousSteps : number = collatz(3 * n + 1);
-            memo.set(n, 1 + previousSteps);
-            return 1 + previousSteps;
+            let previousSteps : number[] = collatz(3 * n + 1);
+            memo.set(n, [n, ...previousSteps]);
+            return [n, ...previousSteps];
         }
     }
 }
