8e036a3fd3ca6891284c3c59cae930e7bcdf8332
Steven Thephsourinthone <zx.steven.t@gmail.com>
2022-11-23 12:19:47 -0500

    Wrote backtracking function

--- Python/subset.py
+++ Python/subset.py
@@ -1,3 +1,17 @@
 def subsets(l):
-    # All subsets of list l
-    pass
+    solutions = []
+    backtrack(0, [], solutions, l)
+    return solutions
+
+def backtrack (ptr, current, solutions, original):
+    if (ptr == len(original)):
+        solutions.append(current.copy())
+        return
+
+    current.append(original[ptr])
+    backtrack(ptr + 1, current, solutions, original)
+
+    current.pop()
+    backtrack(ptr + 1, current, solutions, original)
+
+print (subsets([1,2,3]))
