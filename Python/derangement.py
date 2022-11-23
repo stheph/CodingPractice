31dc40c2d4f6fea11486b68f36fa9a7099f9f407
Steven Thephsourinthone <zx.steven.t@gmail.com>
2022-11-23 12:28:06 -0500

    Added backtracking function; it's permutation but prunes candidates equal to pos

--- Python/derangement.py
+++ Python/derangement.py
@@ -1,3 +1,22 @@
 def derangement(l):
-    # Generate a derangement of list l
-    pass
+    solutions = []
+    backtrack([], solutions, l)
+    return solutions
+
+def backtrack(current, solutions, original):
+    if (len(current) == len(original)):
+        solutions.append(current.copy())
+        return
+
+    candidates = []
+    for el in original:
+        if not el in current:
+            if not el == len(current) + 1:
+                candidates.append(el)
+    
+    for candidate in candidates:
+        current.append(candidate)
+        backtrack(current, solutions, original)
+        current.pop()
+
+print (derangement([1,2,3,4]))
